bb18734562be5277d0ab98e11e750d0ac826eda4
Amit kumar <82734969+Amit998-bit@users.noreply.github.com>
2023-12-12 02:45:35 +0530

    first commit

---
+++ src/views/Starter.js
@@ -0,0 +1,124 @@
+import { Col, Row } from "reactstrap";
+import SalesChart from "../components/dashboard/SalesChart";
+import Feeds from "../components/dashboard/Feeds";
+import ProjectTables from "../components/dashboard/ProjectTable";
+import TopCards from "../components/dashboard/TopCards";
+import Blog from "../components/dashboard/Blog";
+import bg1 from "../assets/images/bg/bg1.jpg";
+import bg2 from "../assets/images/bg/bg2.jpg";
+import bg3 from "../assets/images/bg/bg3.jpg";
+import bg4 from "../assets/images/bg/bg4.jpg";
+
+const BlogData = [
+  {
+    image: bg1,
+    title: "This is simple blog",
+    subtitle: "2 comments, 1 Like",
+    description:
+      "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    btnbg: "primary",
+  },
+  {
+    image: bg2,
+    title: "Lets be simple blog",
+    subtitle: "2 comments, 1 Like",
+    description:
+      "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    btnbg: "primary",
+  },
+  {
+    image: bg3,
+    title: "Don't Lamp blog",
+    subtitle: "2 comments, 1 Like",
+    description:
+      "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    btnbg: "primary",
+  },
+  {
+    image: bg4,
+    title: "Simple is beautiful",
+    subtitle: "2 comments, 1 Like",
+    description:
+      "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    btnbg: "primary",
+  },
+];
+
+const Starter = () => {
+  return (
+    <div>
+      {/***Top Cards***/}
+      <Row>
+        <Col sm="6" lg="3">
+          <TopCards
+            bg="bg-light-success text-success"
+            title="Profit"
+            subtitle="Arrived"
+            earning="231"
+            icon="bi bi-box-arrow-left"
+          />
+        </Col>
+        <Col sm="6" lg="3">
+          <TopCards
+            bg="bg-light-danger text-danger"
+            title="Refunds"
+            subtitle="Total booked"
+            earning="891"
+            icon="bi bi-book-half"
+          />
+        </Col>
+        <Col sm="6" lg="3">
+          <TopCards
+            bg="bg-light-warning text-warning"
+            title="New Project"
+            subtitle="Total check in"
+            earning="456"
+            icon="bi bi-box-arrow-in-right"
+          />
+        </Col>
+        <Col sm="6" lg="3">
+          {/* <TopCards
+            bg="bg-light-info text-into"
+            title="Sales"
+            subtitle="Weekly Sales"
+            earning="210"
+            icon="bi bi-bag"
+          /> */}
+
+          <h4 id="text" style={{color:"rgb(5, 5, 100)",fontWeight:"900"}}>Staff schedule</h4> <a href="/table" style={{textDecoration:"none"}}>See all</a>
+        </Col>
+      </Row>
+      {/***Sales & Feed***/}
+      <Row>
+        <Col sm="6" lg="6" xl="7" xxl="8">
+          <SalesChart />
+        </Col>
+        <Col sm="6" lg="6" xl="5" xxl="4">
+          <Feeds />
+        </Col>
+      </Row>
+      {/***Table ***/}
+      <Row>
+        <Col lg="12">
+          <ProjectTables />
+        </Col>
+      </Row>
+      {/***Blog Cards***/}
+      <Row>
+        {BlogData.map((blg, index) => (
+          <Col sm="6" lg="6" xl="3" key={index}>
+            <Blog
+              image={blg.image}
+              title={blg.title}
+              subtitle={blg.subtitle}
+              text={blg.description}
+              color={blg.btnbg}
+            />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
+
+export default Starter;
